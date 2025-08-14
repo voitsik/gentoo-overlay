70c42eb90bc562603028aa76e0b499ca4fd9f3ba
Petr Voitsik <voitsik@gmail.com>
2025-08-14 15:06:48 +0300

    dev-python/astroquery: add 0.4.10
    Signed-off-by: Petr Voitsik <voitsik@gmail.com>

---
+++ dev-python/astroquery/files/conftest.py
@@ -0,0 +1,62 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+import os
+from pathlib import Path
+
+from astropy.utils import minversion
+import numpy as np
+import pytest
+from pytest_astropy_header.display import (PYTEST_HEADER_MODULES,
+                                           TESTED_VERSIONS)
+
+
+# Keep this until we require numpy to be >=2.0
+if minversion(np, "2.0.0.dev0+git20230726"):
+    np.set_printoptions(legacy="1.25")
+
+
+def pytest_configure(config):
+    config.option.astropy_header = True
+
+    PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
+    PYTEST_HEADER_MODULES['regions'] = 'regions'
+    PYTEST_HEADER_MODULES['pyVO'] = 'pyvo'
+    PYTEST_HEADER_MODULES['mocpy'] = 'mocpy'
+    PYTEST_HEADER_MODULES['astropy-healpix'] = 'astropy_healpix'
+    PYTEST_HEADER_MODULES['vamdclib'] = 'vamdclib'
+
+    del PYTEST_HEADER_MODULES['h5py']
+    del PYTEST_HEADER_MODULES['Scipy']
+    del PYTEST_HEADER_MODULES['Pandas']
+
+    # keyring doesn't provide __version__ any more
+    # PYTEST_HEADER_MODULES['keyring'] = 'keyring'
+
+    # add '_testrun' to the version name so that the user-agent indicates that
+    # it's being run in a test
+    from astroquery import version
+    version.version += '_testrun'
+
+    TESTED_VERSIONS['astroquery'] = version.version
+    TESTED_VERSIONS['astropy_helpers'] = version.astropy_helpers_version
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--alma-site',
+        action='store',
+        default='almascience.eso.org',
+        help='ALMA site (almascience.nrao.edu, almascience.eso.org or '
+             'almascience.nao.ac.jp for example)'
+    )
+
+
+@pytest.fixture(scope='function')
+def tmp_cwd(tmp_path):
+    """Perform test in a pristine temporary working directory."""
+    old_dir = Path.cwd()
+    os.chdir(tmp_path)
+    try:
+        yield tmp_path
+    finally:
+        os.chdir(old_dir)
